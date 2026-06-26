9a3d91ef0fe2979936010fbe24d0dd16194b9974
front-lucy <101608868+Garam-Leee@users.noreply.github.com>
2026-06-26 11:18:46 +0900

    feat: setup book api
    Axios와 카카오 도서 검색 API를 연동하고, 응답 타입 및 API 요청 함수 구현

---
+++ src/types/book.ts
@@ -0,0 +1,32 @@
+export interface Book {
+  title: string;
+  contents: string;
+  url: string;
+  isbn: string;
+  datetime: string;
+  authors: string[];
+  publisher: string;
+  translators: string[];
+  price: number;
+  sale_price: number;
+  thumbnail: string;
+  status: string;
+}
+
+export interface BookSearchResponse {
+  meta: {
+    total_count: number;
+    pageable_count: number;
+    is_end: boolean;
+  };
+
+  documents: Book[];
+}
+
+export interface SearchBooksParams {
+  query: string;
+  page?: number;
+  size?: number;
+  sort?: "accuracy" | "latest";
+  target?: "title" | "isbn" | "publisher" | "person";
+}
