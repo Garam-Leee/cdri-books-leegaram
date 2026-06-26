925d47c2fb7357ebee7575644968748a94cf03b5
front-lucy <101608868+Garam-Leee@users.noreply.github.com>
2026-06-26 11:06:46 +0900

    chore: setup query provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import QueryProvider from "./providers/QueryProvider";
 import ThemeProvider from "./providers/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryProvider>
+        <App />
+      </QueryProvider>
     </ThemeProvider>
   </StrictMode>,
 );
